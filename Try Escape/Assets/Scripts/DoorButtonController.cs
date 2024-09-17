0156b1dfb4eaf6819840616a1e318ce58160eb16
Yunnna005 <annakovalenko0500@gmail.com>
2024-09-17 16:13:47 +0100

    Added lighting, door amination, door open botton Signed-off-by: Yunnna005 <annakovalenko0500@gmail.com>

--- Try Escape/Assets/Scripts/DoorButtonController.cs
+++ Try Escape/Assets/Scripts/DoorButtonController.cs
@@ -1,14 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorButtonController : MonoBehaviour
 {
-    public float detectionRadius = 10f; 
+    public float detectionRadius = 10f;
+    MeshRenderer meshRenderer;
+    private float timeRemaining = 0;
+    private bool isPressed = false;
 
     void OnMouseDown()
     {
+        meshRenderer = GetComponent<MeshRenderer>();
+        isPressed = true;
+        timeRemaining = 10f;
+        ChangeButtonColor(meshRenderer);
         OpenNearestDoor();
+    }
+
+    private void Update()
+    {
+        if (isPressed)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+
+            if (timeRemaining <= 0)
+            {
+                isPressed = false;
+                ChangeButtonColor(meshRenderer);
+            }
+        }
+    }
+
+    private void ChangeButtonColor(MeshRenderer mr)
+    {
+        if (isPressed)
+        {
+            mr.material.color = Color.green;
+        }
+        else
+        {
+            mr.material.color = Color.white;
+        }
     }
 
     void OpenNearestDoor()
